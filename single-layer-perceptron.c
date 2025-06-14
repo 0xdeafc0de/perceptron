a3463591158a60810dc853066e187fb4e8921801
Sanjay Pingal <sanjaypingal@gmail.com>
2025-06-15 02:22:14 +0530

    output calculation

--- single-layer-perceptron.c
+++ single-layer-perceptron.c
@@ -30,3 +30,39 @@ void init_perceptron(Neuron *n) {
     n->bias = ((double)rand() / RAND_MAX) * 2 - 1;
 }
 
+// calculate neuron output for a given set of input
+double calc_output(Neuron *n, double *inp, int n_inp) {
+    double z = 0.0;
+    int i;
+    for (i = 0; i < n_inp; i++) {
+        z += n->weights[i] * inp[i];
+    }
+    z += n->bias;
+    n->output = sigmoid(z);
+    return n->output;
+}
+
+///////// Training function////////
+// Inputs
+// n   ->   pointer to neuron/perceptron
+// inp ->   pointer to input features
+// n_inp -> number of input features
+// tgt ->   target value
+// lr  ->   learning rate
+void train_perceptron(Neuron *n, double *inp, int n_inp, double tgt, double lr) {
+}
+
+void print_neuron(Neuron *n) {
+    printf("weights: [%.3f, %.3f, %.3f], Bias: %.3f\n",
+        n->weights[0], n->weights[1], n->weights[2], n->bias);
+}
+
+int main() {
+    srand(time(NULL));
+
+    Neuron neuron;
+    init_perceptron(&neuron);
+    print_neuron(&neuron);
+
+    return 0;
+}
